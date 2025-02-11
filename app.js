9923080c1d8160bb5239660c3164f7f9e6d8511e
Jennikelly <Jenniferkrc2@gmail.com>
2025-02-11 07:27:51 -0300

    Criando a função atualizarLista()

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-let nomes = [];
+let amigos = [];
+
+// Percorro o array amigos e adiciona cada nome como um elemento <li> dentro de uma lista HTML.
+function atualizarLista(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        // Criando um elemento do tipo <li>
+        let item = document.createElement('li');
+        // Inserindo o texto
+        item.textContent = amigos[i];
+        // Adicionando à lista HTML
+        lista.appendChild(item);
+    }
+}
 
 function adicionarAmigo(){
+    // Obtendo o valor escrito
     let nomeInserido = document.querySelector('input').value;
 
     if(nomeInserido == ''){
         alert('Erro: Por favor, insira um nome.');
-    } else{
-        nomes.push(nomeInserido);
-        console.log(nomes);
+    // Evitando duplicatas
+    } else if (amigos.includes(nomeInserido)){
+        alert('Erro, este nome já existe!');
+    } else {
+        // Adicionando o novo nome à lista amigos
+        amigos.push(nomeInserido);
+        console.log(amigos);
+        // Limpando a caixa de escrita
         limpar = document.querySelector('input')
         limpar.value = '';
+        // Para atualizar a lista HTML
+        atualizarLista();
     }
 }
-    
+
+
+
